a1e7ffd112ba9db42c21903d58403128da67a7ce
Gregory Casamento <greg.casamento@gmail.com>
2023-08-26 09:44:50 -0400

    Initial Commit

---
+++ NSButton_switchXIB/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSButton_switchXIB
+//
+//  Created by Gregory John Casamento on 8/26/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSButton_switchXIB/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  NSButton_switchXIB
+//
+//  Created by Gregory John Casamento on 8/26/23.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
